1debef6e4dcb725e39e0a388ad5d42ecf751278f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-16 01:23:33 +0600

    adding something new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react';
+import './App.css'
+import Button from './components/UI/Button/Button';
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+
+const toggleParagraphHandler=() => {
+  setShowParagraph((prevShowParagraph)=> !prevShowParagraph)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" >
+         <h1>Hi there!</h1>
+         {showParagraph &&<p>this is new</p>}
+         <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
