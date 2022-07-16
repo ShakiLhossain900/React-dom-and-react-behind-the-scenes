b8ca66070f7aa506eeca36a6cf6a0a8a196ada0f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-16 09:30:30 +0600

    adding something

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import {useState} from 'react';
-import './App.css'
-import Button from './components/UI/Button/Button';
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false)
+  const [showParagraph, setShowParagraph] = useState(false);
 
-const toggleParagraphHandler=() => {
-  setShowParagraph((prevShowParagraph)=> !prevShowParagraph)
-}
+  const toggleParagraphHandler = () => {
+    setShowParagraph((shakil) => !shakil);
+  };
 
   return (
-    <div className="app" >
-         <h1>Hi there!</h1>
-         {showParagraph &&<p>this is new</p>}
-         <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>this is new</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
