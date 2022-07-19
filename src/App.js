3a02be29911b69429492378460b7a2fac36fdcd6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-20 00:01:04 +0600

    adding something new

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@
 import { useState, useCallback } from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
- import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
+ //import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
   console.log("App RUNNING");
 
   const toggleParagraphHandler = useCallback( () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph); //if  i am using this setshowparagraph er vitor akta function use kore argument pass kori !use korle same argument y tokhon akbar show korbe and arekbar click korle hide korbe show
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph); //if  i am using this setshowparagraph er vitor akta function use kore argument pass kori
+    // !use korle same argument y tokhon akbar show korbe and arekbar click korle hide korbe show
     //setShowParagraph(true) // ami jodi setShowParagraph true kore dei tahole only one time shoow korbe but hide korte parbe na
   },[]);
 
@@ -20,8 +21,8 @@ function App() {
     <div className="app">
       <h1>Hi there!</h1>
       {/* {showParagraph && <p>this is new</p>} */}
-      <DemoOutput show={false} />
-      {/* <p>{showParagraph ? 'this is new!' : ''}</p> */}
+      {/* <DemoOutput show={false} /> */}
+      <p>{showParagraph ? 'this is new!' : ''}</p>
       <Button buttonOnClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
